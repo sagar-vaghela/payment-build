bad773c53732d710ffa162d31a5524d404d27202
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2023-04-11 15:41:45 +0530

    fixed login api call

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,1 +1,6 @@
-export const loginUrl = '/auth/login';
+import { loginUrl } from "src/lib/constants";
+import { createAxiosFor } from "../axios";
+
+export const loginApi = (payload: any) => {
+    return createAxiosFor.post(`${loginUrl}`, payload)
+  };
--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-const { NEXT_PUBLIC_API_URL } = process.env;
 
 export const createAxiosFor = axios.create({
-  baseURL: NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
