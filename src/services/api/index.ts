10c09e11e0ca1e47676f9e300e651409317c106c
Vaghela Sagar <sagar19vaghela.sv@gmail.com>
2023-04-11 17:28:44 +0530

    fixed bearer token in user api

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,10 +1,21 @@
 import { loginUrl, userCurrentUrl } from "src/lib/constants";
 import { createAxiosFor } from "../axios";
 
+const ACCESS_TOKEN_KEY = "access_token";
+
 export const loginApi = (payload: any) => {
   return createAxiosFor.post(`${loginUrl}`, payload);
 };
 
 export const userCurrentAPi = () => {
-  return createAxiosFor.get(`${userCurrentUrl}`);
+  let access_token: string | null = "";
+
+  if (typeof window !== "undefined") {
+    access_token = localStorage.getItem(ACCESS_TOKEN_KEY);
+  }
+  return createAxiosFor.get(`${userCurrentUrl}`, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
 };
--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,14 +1,5 @@
 import axios from "axios";
 
-const ACCESS_TOKEN_KEY = 'access_token';
-
-let access_token;
-if (typeof window !== "undefined") {
-  access_token = localStorage.getItem(ACCESS_TOKEN_KEY);
-}
-console.log("access_token", access_token);
-
 export const createAxiosFor = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: { Authorization: `Bearer ${access_token}`},
 });
