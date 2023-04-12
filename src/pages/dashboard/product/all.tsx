8ddc7393cd80e3e13a104680fca4a681bb6b7c60
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2023-04-12 17:58:14 +0530

    table fetch

--- src/pages/dashboard/product/all.tsx
+++ src/pages/dashboard/product/all.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { Page as PageType } from 'src/types/page';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard';
+import { useAuth } from 'src/hooks/use-auth';
+import { productsListAPI } from 'src/services/api';
 
 const Page: PageType = () => {
-    return(<div>All</div>)
+
+  const auth = useAuth();
+
+  useEffect(() => {
+    const callApi = async () => {
+      const productsList = await productsListAPI('list');
+      console.log("productsList---response---", productsList);
+
+    };
+    if (auth?.token?.access_token) {
+      callApi();
+    }
+  }, [auth.token.access_token]);
+
+
+  return (
+
+    <div>
+
+      All
+
+    </div>
+
+  )
 }
 
 Page.getLayout = (page) => (
-    <DashboardLayout>
-      {page}
-    </DashboardLayout>
-  );
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
 
 export default Page;
